b08e95fd52d913be52ecf7b0c8897f3a9c64131d
NiclO1337 <niclas.hugdahl@gmail.com>
2026-09-03 22:29:02 +0200

    move level1 code to its own file

---
+++ Week36/level1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week36
+{
+    internal class Level1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\nEnter products to save them, type \"exit\" to finish.");
+
+            List<string> products = [];
+
+            while (true)
+            {
+                Console.Write("Product: ");
+                string? input = Console.ReadLine();
+
+                if (input?.Trim().ToLower() == "exit")
+                {
+                    break;
+                }
+                if (input.Trim().Length > 0) { products.Add(input.Trim()); }
+            }
+
+            Console.WriteLine("\nProducts entered:\n");
+            Console.WriteLine(String.Join("\n", products));
+        }
+    }
+}
