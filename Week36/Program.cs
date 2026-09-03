3c6c3cd507b77f8fab1d5dd7cad3a7f067467b3b
NiclO1337 <niclas.hugdahl@gmail.com>
2026-09-03 22:29:16 +0200

    Add program to handle running different levels

--- Week36/Program.cs
+++ Week36/Program.cs
@@ -1,26 +1,50 @@
-﻿Console.WriteLine("" +
+﻿using Week36;
+
+Action[] levels =
+[
+    Level1.Run
+];
+
+Console.WriteLine("" +
     "------------------------------------" +
-    "\n   PRODUCT LIST MANAGER - LEVEL 1\n" +
+    "\n   Mini project week 36\n" +
     "------------------------------------");
 
-Console.WriteLine("\nEnter products to save them, type \"exit\" to finish.");
+Console.WriteLine($"There are {levels.Length} levels avalible to run here.");
+Console.Write($"Enter 1 - {levels.Length}, or 'a' to run all: ");
+var input = Console.ReadLine();
 
-List<string> products = [];
-
-while (true)
+if (input == "a")
 {
-    Console.Write("Product: ");
-    string? input = Console.ReadLine();
-
-    if (input?.Trim().ToLower() == "exit")
+    for (int i = 0; i < levels.Length; i++)
     {
-        break;
+        Console.WriteLine("" +
+            "------------------------------------" +
+            $"\n   PRODUCT LIST MANAGER - LEVEL {i + 1}\n" +
+            "------------------------------------");
+
+        levels[i]();
+        if (i < levels.Length - 1)
+        {
+            Console.WriteLine("\nPress ENTER for next level.");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("\nPress any key to continue...");
+        }
     }
-    if (input.Trim().Length > 0) { products.Add(input.Trim()); }
 }
+else if (int.TryParse(input, out int n) && n >= 1 && n <= levels.Length)
+{
+    Console.WriteLine("" +
+        "------------------------------------" +
+        $"\n   PRODUCT LIST MANAGER - LEVEL {n}\n" +
+        "------------------------------------");
+    levels[n - 1]();
+    Console.WriteLine("\nPress any key to continue...");
+}
+else
+    Console.WriteLine("No such level.");
 
-Console.WriteLine("\nProducts entered:\n");
-Console.WriteLine(String.Join("\n", products));
-
-Console.WriteLine("\nPress any key to continue...");
 Console.ReadKey();
