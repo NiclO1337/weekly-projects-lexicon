74e893a0375cf09c04f4215d7b3c4567b4a59029
NiclO1337 <niclas.hugdahl@gmail.com>
2026-09-03 16:26:48 +0200

    Add solution for level1

--- Week36/Program.cs
+++ Week36/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the product list manager!\n");
+
+Console.WriteLine("Enter products to save them, type \"exit\" to finish.");
+
+List<string> products = [];
+
+while (true)
+{   
+    Console.Write("Product: ");
+    string? input = Console.ReadLine();
+
+    if (input?.Trim().ToLower() == "exit")
+    {
+        break;
+    }
+    products.Add(input.Trim());
+}
+
+Console.WriteLine("\nProducts entered:\n");
+Console.WriteLine(String.Join("\n", products));
+
+Console.WriteLine("\nPress any key to continue...");
+Console.ReadKey();
