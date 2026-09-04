7aea858d015d2487d1e3f16790b4e393aece7572
NiclO1337 <niclas.hugdahl@gmail.com>
2026-09-04 14:33:00 +0200

    refactor: move input logic back to Run() function
    validate function now only validates as intended

--- Week36/Level3.cs
+++ Week36/Level3.cs
@@ -15,15 +15,6 @@ namespace Week36
 
             List<string> productList = [];
 
-            ValidateProductInput(productList);
-
-            productList.Sort();
-
-            Console.WriteLine("\nProducts entered (sorted A-Z):\n");
-            Console.Write("- "); Console.WriteLine(String.Join("\n- ", productList));
-        }
-        private static List<string> ValidateProductInput(List<string> products)
-        {
             while (true)
             {
                 Console.Write("\nProduct: ");
@@ -37,54 +28,60 @@ namespace Week36
                 }
                 else if (trimmedInput.Equals("exit", StringComparison.OrdinalIgnoreCase))
                 {
-                    return products;
+                    break;
                 }
 
-                string[]? splitInput = trimmedInput?.Split("-");
+                (bool isValid, string? errorMessage) = ValidateProductInput(trimmedInput);
 
-                if (splitInput?.Length != 2)
+
+                if (isValid)
                 {
-                    Console.WriteLine("Error: Product name requires 1 and only 1 dash ( - ).");
-                } 
-                else
+                    productList.Add(trimmedInput!);
+                    Console.WriteLine("Successfully added product.");
+                }
+                else if (!string.IsNullOrEmpty(errorMessage))
                 {
-                    bool isLetters = splitInput[0].All(char.IsLetter);
-                    bool isNumbers = int.TryParse(splitInput[1], out int numbers);
-                    bool isValid = true;
-
-                    foreach (string part in splitInput)
-                    {
-                        Console.WriteLine(part);
-                    }
-
-                    if (!isLetters)
-                    {
-                        Console.WriteLine("Error: Left side of the dash ( - ) must only contain letters.");
-                        isValid = false;
-                    }
-                    if (splitInput[0].Length < 1 || splitInput[0].Length > 5)
-                    {
-                        Console.WriteLine("Error: Must only use between 1 and 5 letters.");
-                        isValid = false;
-                    }
-                    if (!isNumbers)
-                    {
-                        Console.WriteLine("Error: Right side of the dash ( - ) must only contain numbers.");
-                        isValid = false;
-                    }
-                    if (numbers < 200 || numbers > 500)
-                    {
-                        Console.WriteLine("Error: Number must be in valid range between 200 and 500.");
-                        isValid = false;
-                    }
-
-                    if (isValid)
-                    {
-                        products.Add(trimmedInput!);
-                        Console.WriteLine("Successfully added product.");
-                    }
-                }                
+                    Console.WriteLine(errorMessage);
+                }
             }
+
+            productList.Sort();
+
+            Console.WriteLine("\nProducts entered (sorted A-Z):\n");
+            Console.Write("- "); Console.WriteLine(String.Join("\n- ", productList));
+        }
+        private static (bool, string?) ValidateProductInput(string input)
+        {
+            string errorMessage = "";
+            string[]? splitInput = input?.Split("-");
+
+            if (splitInput?.Length != 2)
+            {
+                errorMessage += "Error: Product name requires 1 and only 1 dash ( - ).\n";
+            } 
+                
+            bool isLetters = splitInput![0].All(char.IsLetter) && !string.IsNullOrEmpty(splitInput[0]);
+            bool isNumbers = int.TryParse(splitInput[1], out int numbers);
+            
+            if (!isLetters)
+            {
+                errorMessage += "Error: Left side of the dash ( - ) must only contain letters.\n";
+            }
+            if (splitInput[0].Length < 1 || splitInput[0].Length > 5)
+            {
+                errorMessage += "Error: Must only use between 1 and 5 letters.\n";
+            }
+            if (!isNumbers)
+            {
+                errorMessage += "Error: Right side of the dash ( - ) must only contain numbers.\n";
+            }
+            if (numbers < 200 || numbers > 500)
+            {
+                errorMessage += "Error: Number must be in valid range between 200 and 500.\n";
+            }
+
+            bool isValid = string.IsNullOrEmpty(errorMessage);
+            return (isValid, isValid ? null : errorMessage.TrimEnd());
         }
     }
 }
